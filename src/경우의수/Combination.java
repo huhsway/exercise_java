ee7c399b4505ddf8c8c15e7631d11bd0dfd85f56
huhsway <seunghoe9542@naver.com>
2023-12-13 22:59:09 +0900

    순열, 조합 함수 수정

--- src/경우의수/Combination.java
+++ src/경우의수/Combination.java
@@ -4,20 +4,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 class Combination {
-    public int solution(int[] arr) {
-        int answer = 0;
-
-        int[] comArr = new int[2];
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        Combination(arr.length, 2, comArr, 0, 0, arr, result);
-
-        for (int i = 0; i < result.size(); i++){
-            System.out.println(result.get(i));
-        }
-
-        return answer;
-    }
-
     public static void Combination(int n, int r, int[] comArr, int index, int target, int[] arr, ArrayList<ArrayList<Integer>> result) {
         if (r == 0) {
             ArrayList<Integer> temp = new ArrayList<Integer>();
@@ -37,7 +23,14 @@ class Combination {
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6};
-        Combination sol = new Combination();
-        System.out.println(sol.solution(arr));
+        int answer = 0;
+
+        int[] comArr = new int[2];
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        Combination(arr.length, 2, comArr, 0, 0, arr, result);
+
+        for (int i = 0; i < result.size(); i++){
+            System.out.println(result.get(i));
+        }
     }
 }
--- src/경우의수/Permutation.java
+++ src/경우의수/Permutation.java
@@ -4,24 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 class Permutation {
-    public int solution(int[] arr) {
-        int answer = 0;
-
-        int[] perArr = new int[arr.length];
-        boolean[] perCheck = new boolean[arr.length];
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-
-        Permutation(arr.length, arr.length, 0, perArr, perCheck, arr, result);
-
-        for (int i = 0; i < result.size(); i++){
-            System.out.println(result.get(i));
-        }
-
-        return answer;
-    }
-
     public static void Permutation(int n, int r, int depth, int[] perArr, boolean[] perCheck, int[] arr, ArrayList<ArrayList<Integer>> result){
-
         if (depth == r) {
             ArrayList<Integer> temp = new ArrayList<Integer>();
             for (int i = 0; i < perArr.length; i++) {
@@ -45,7 +28,16 @@ class Permutation {
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4};
-        Permutation sol = new Permutation();
-        System.out.println(sol.solution(arr));
+        int answer = 0;
+
+        int[] perArr = new int[arr.length];
+        boolean[] perCheck = new boolean[arr.length];
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+        Permutation(arr.length, arr.length, 0, perArr, perCheck, arr, result);
+
+        for (int i = 0; i < result.size(); i++){
+            System.out.println(result.get(i));
+        }
     }
 }
