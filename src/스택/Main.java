7d03c453dc9388a890178436a2be91cf84fce0a3
huhsway <seunghoe9542@naver.com>
2023-12-13 22:53:49 +0900

    Deque를 이용한 Stack으로 변경

--- src/스택/Main.java
+++ src/스택/Main.java
@@ -5,21 +5,21 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Stack<Integer> s = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
 
         // 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
-        s.push(5);
-        s.push(2);
-        s.push(3);
-        s.push(7);
-        s.pop();
-        s.push(1);
-        s.push(4);
-        s.pop();
+        stack.push(5);
+        stack.push(2);
+        stack.push(3);
+        stack.push(7);
+        stack.pop();
+        stack.push(1);
+        stack.push(4);
+        stack.pop();
         // 스택의 최상단 원소부터 출력
-        while (!s.empty()) {
-            System.out.println(s.peek());
-            s.pop();
+        while (!stack.isEmpty()) {
+            System.out.println(stack.peek());
+            stack.pop();
         }
     }
 
