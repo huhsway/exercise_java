178df5a99f1de2deaf8b2222958cad9d6d995468
huhsway <seunghoe9542@naver.com>
2024-01-14 18:21:37 +0900

    20240114

--- src/국영수/Main.java
+++ src/국영수/Main.java
@@ -1,9 +1,7 @@
 package 국영수;
 
 import java.util.*;
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.io.*;
 
 // https://velog.io/@xxwb__/%EC%9D%B4%EA%B2%83%EC%9D%B4-%EC%BD%94%EB%94%A9-%ED%85%8C%EC%8A%A4%ED%8A%B8%EB%8B%A4-%EC%A0%95%EB%A0%AC-%EA%B5%AD%EC%98%81%EC%88%98
 
@@ -12,64 +10,75 @@ import java.util.stream.Collectors;
 //    private String name;
 //    private int kor;
 //    private int eng;
-//    private int m;
+//    private int math;
 //
-//    public Student(String name, int kor, int eng, int m) {
+//    public Student(String name, int kor, int eng, int math) {
 //        this.name = name;
 //        this.kor = kor;
 //        this.eng = eng;
-//        this.m = m;
+//        this.math = math;
 //    }
 //
-//    /*
-//    [ 정렬 기준 ]
-//    1) 두 번째 원소를 기준으로 내림차순 정렬
-//    2) 두 번째 원소가 같은 경우, 세 번째 원소를 기준으로 오름차순 정렬
-//    3) 세 번째 원소가 같은 경우, 네 번째 원소를 기준으로 내림차순 정렬
-//    4) 네 번째 원소가 같은 경우, 첫 번째 원소를 기준으로 오름차순 정렬
-//    */
-//
 //    public String getName() {
-//        return this.name; }
+//        return this.name;
+//    }
 //
-//    @Override
 //    public int compareTo(Student other) {
-//        if (this.kor == other.kor && this.eng == other.eng && this.m == other.m) {
+//        if (this.kor == other.kor && this.eng == other.eng && this.math == other.math) {
 //            return this.name.compareTo(other.name);
 //        }
 //        if (this.kor == other.kor && this.eng == other.eng) {
-//            return Integer.compare(other.m, this.m);
+//            return other.math - this.math;
 //        }
 //        if (this.kor == other.kor) {
-//            return Integer.compare(this.eng, other.eng);
+//            return this.eng - other.eng;
 //        }
-//        return Integer.compare(other.kor, this.kor);
+//        return other.kor - this.kor;
 //    }
 //
 //}
 //
-//public class Main {
+///**
+// *
+// BufferedReader와 StringTokenizer를 Scanner에 선호하는 이유는 다음과 같습니다:
 //
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
+// 성능: BufferedReader와 StringTokenizer는 대량의 입력을 더 효율적으로 처리할 수 있으며, 더 빠르게 입력을 파싱할 수 있습니다. 특히, 대규모 입력을 처리할 때 Scanner보다 빠를 수 있습니다.
+//
+// 메모리 사용: Scanner는 내부 버퍼를 사용하며, 이로 인해 메모리 사용량이 높을 수 있습니다. 반면에 BufferedReader와 StringTokenizer는 더 적은 메모리를 사용합니다.
+//
+// 문자열 처리: StringTokenizer는 문자열을 더 쉽게 분할할 수 있습니다. 특히, 공백 문자 또는 다른 구분자를 기준으로 문자열을 나눌 때 유용합니다.
+//
+// 유연성: BufferedReader와 StringTokenizer는 다양한 입력 소스 (파일, 표준 입력, 문자열 등)에서 사용할 수 있으며, 개발자가 더 많은 제어를 할 수 있습니다.
+//
+// 에러 처리: BufferedReader는 예외 처리를 더 쉽게 할 수 있도록 지원합니다. Scanner는 예외 처리를 잡기 어려울 수 있습니다.
+//
+// 물론 Scanner도 편리한 입력 처리 도구 중 하나이며, 간단한 입력 처리에는 충분할 수 있습니다. 그러나 대량의 데이터나 성능이 중요한 상황에서는 BufferedReader와 StringTokenizer를 사용하는 것이 일반적으로 더 효율적입니다.
+// */
+//class Main {
+//
+//    public static void main(String[] args) throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//        int n = Integer.parseInt(br.readLine());
 //
 //        ArrayList<Student> students = new ArrayList<>();
 //        for (int i = 0; i < n; i++) {
-//            String name = sc.next();
-//            int kor = sc.nextInt();
-//            int eng = sc.nextInt();
-//            int m = sc.nextInt();
-//            students.add(new Student(name, kor, eng, m));
+//            st = new StringTokenizer(br.readLine());
+//            String name = st.nextToken();
+//            int kor = Integer.parseInt(st.nextToken());
+//            int eng = Integer.parseInt(st.nextToken());
+//            int math = Integer.parseInt(st.nextToken());
+//            students.add(new Student(name, kor, eng, math));
 //        }
 //
 //        Collections.sort(students);
 //
-//        // 정렬된 학생 정보에서 이름만 출력
 //        for (int i = 0; i < n; i++) {
 //            System.out.println(students.get(i).getName());
 //        }
 //    }
+//
 //}
 
 class Main {
