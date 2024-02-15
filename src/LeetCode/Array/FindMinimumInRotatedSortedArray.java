dbf58a4194033f5ec9dafae613ac45382cd701d1
huhsway <seunghoe9542@naver.com>
2024-02-15 21:20:55 +0900

    20240215

--- src/LeetCode/Array/FindMinimumInRotatedSortedArray.java
+++ src/LeetCode/Array/FindMinimumInRotatedSortedArray.java
@@ -3,24 +3,23 @@ package LeetCode.Array;
 public class FindMinimumInRotatedSortedArray {
 
     public int findMin(int[] nums) {
-        int i = 0;
-        int j = nums.length - 1;
 
-        while (i < j) {
-            int m = (i + j) / 2;
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
 
-            if (nums[m] > nums[j]) {
-                // 중간 지점의 값이 오른쪽 끝 지점의 값보다 크다면, 회전된 부분이 왼쪽 있음.
-                // 최솟값은 중간 지점의 오른쪽에 있으므로 i를 중간 지점 다음으로 이동합니다.
-                i = m + 1;
+        while (left < right) {
+            mid = (left + right) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
             } else {
-                // 중간 지점의 값이 오른쪽 끝 지점의 값보다 작거나 같다면, 회전된 부분이 오른쪽에 있음.
-                // 최솟값은 중간 지점의 왼쪽에 있으므로 j를 중간 지점으로 이동합니다.
-                j = m;
+                right = mid;
             }
         }
 
-        return nums[i];
+        return nums[left];
+
     }
 
     public static void main(String[] args) {
