20c72eb6feb505ddaae2d4dc80cdea537144618d
huhsway <seunghoe9542@naver.com>
2024-04-06 11:36:29 +0900

    20240406

--- src/자료구조/우선순위큐/PriorityQueue.java
+++ src/자료구조/우선순위큐/PriorityQueue.java
@@ -1,5 +1,44 @@
 package 자료구조.우선순위큐;
 
+//public class Main {
+//    public static void main(String[] args) {
+//        // MyClass 인스턴스를 저장하는 최대힙 PriorityQueue 생성
+//        PriorityQueue<MyClass> maxHeap = new PriorityQueue<>();
+//
+//        // PriorityQueue에 MyClass 인스턴스 추가
+//        maxHeap.push(new MyClass(10));
+//        maxHeap.push(new MyClass(5));
+//        maxHeap.push(new MyClass(20));
+//        maxHeap.push(new MyClass(1));
+//
+//        System.out.println("PriorityQueue에서 요소를 제거하며 출력:");
+//        while (maxHeap.peek() != null) {
+//            MyClass removed = maxHeap.pop();
+//            System.out.println(removed.getValue());
+//        }
+//    }
+//}
+
+// // 사용자 정의 클래스 MyClass
+//class MyClass implements Comparable<MyClass> {
+//    private int value; // 비교를 위한 값
+//
+//    public MyClass(int value) {
+//        this.value = value;
+//    }
+//
+//    public int getValue() {
+//        return this.value;
+//    }
+//
+//    // MyClass 객체들을 비교하기 위한 compareTo 메소드
+//    @Override
+//    public int compareTo(MyClass other) {
+//        // 최대힙을 위해 비교 방식을 반대로 함
+//        return other.value - this.value;
+//    }
+//}
+
 public class PriorityQueue<T extends Comparable<T>> {
     private Object[] heap;
     private int size;
@@ -9,20 +48,21 @@ public class PriorityQueue<T extends Comparable<T>> {
         this.heap = new Object[DEFAULT_CAPACITY];
         this.size = 0;
     }
+
     private void heapifyUp() {
         int index = size - 1;
         while (index > 0) {
             int parentIndex = (index - 1) / 2;
-            if (((T) heap[parentIndex]).compareTo((T) heap[index]) > 0) { // 최대힙: >, 최소힙: <
+            if (((T) heap[parentIndex]).compareTo((T) heap[index]) < 0) { // < 0 최대 힙, > 0 최소 힙
+                // swap
+                Object temp = heap[index];
+                heap[index] = heap[parentIndex];
+                heap[parentIndex] = temp;
+
+                index = parentIndex;
+            } else {
                 break;
             }
-
-            // swap
-            Object temp = heap[index];
-            heap[index] = heap[parentIndex];
-            heap[parentIndex] = temp;
-
-            index = parentIndex;
         }
     }
 
@@ -33,12 +73,10 @@ public class PriorityQueue<T extends Comparable<T>> {
             int rightChildIndex = 2 * newIndex + 2;
             int index = newIndex;
 
-            // 왼쪽 자식 노드와 비교하여 더 작은 값을 가진 자식 노드를 찾음
-            if (leftChildIndex < size && ((T) heap[leftChildIndex]).compareTo((T) heap[index]) < 0) {
+            if (leftChildIndex < size && ((T) heap[leftChildIndex]).compareTo((T) heap[index]) > 0) { // > 0 최대 힙, < 0 최소 힙
                 index = leftChildIndex;
             }
-            // 오른쪽 자식 노드와 비교하여 더 작은 값을 가진 자식 노드를 찾음
-            if (rightChildIndex < size && ((T) heap[rightChildIndex]).compareTo((T) heap[index]) < 0) {
+            if (rightChildIndex < size && ((T) heap[rightChildIndex]).compareTo((T) heap[index]) > 0) { // > 0 최대 힙, < 0 최소 힙
                 index = rightChildIndex;
             }
 
@@ -55,7 +93,6 @@ public class PriorityQueue<T extends Comparable<T>> {
             }
         }
     }
-
 
     public T peek() {
         if (size == 0) {
