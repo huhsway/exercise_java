1eb10568e18006f994712bc888fe17cf8d6d73d0
허승회 <35622185+huhsway@users.noreply.github.com>
2024-04-09 16:11:43 +0900

    Update LongestRepeatingCharacterReplacement.java

--- src/LeetCode/String/LongestRepeatingCharacterReplacement.java
+++ src/LeetCode/String/LongestRepeatingCharacterReplacement.java
@@ -4,26 +4,21 @@ import java.util.*;
 
 public class LongestRepeatingCharacterReplacement {
     public int characterReplacement(String s, int k) {
-        Map<Character, Integer> count = new HashMap<>(); // 문자와 해당 문자의 개수를 저장할 맵
-        int maxCount = 0; // 최대 반복 문자 개수
-        int maxLength = 0; // 최대 부분 문자열 길이
-        int left = 0; // 윈도우의 왼쪽 인덱스
+        Map<Character, Integer> count = new HashMap<>();
+        int maxLength = 0;
+        int maxCount = 0;
+        int left = 0;
         int right = 0;
-        
-        for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-            count.put(c, count.getOrDefault(c, 0) + 1); // 해당 문자의 개수 증가
-            maxCount = Math.max(maxCount, count.get(c)); // 최대 반복 문자 개수 갱신
 
-            // 윈도우 내의 교체 가능한 문자 개수 계산
-            int replaceCount = right - left + 1 - maxCount;
-            if (replaceCount > k) {
-                // 윈도우 내의 문자 개수 초과 시, 윈도우의 왼쪽을 이동하여 조절
-                count.put(s.charAt(left), count.get(s.charAt(left)) - 1); // 윈도우에서 가장 왼쪽에 있는 문자 개수 감소
-                left++; // 윈도우의 왼쪽 이동
+        for (right = 0; right < s.length(); right++) {
+            count.put(s.charAt(right), count.getOrDefault(s.charAt(right), 0) + 1);
+            maxCount = Math.max(maxCount, count.get(s.charAt(right)));
+
+            if (right - left + 1 - maxCount > k) {
+                count.put(s.charAt(left), count.get(s.charAt(left)) - 1);
+                left++;
             }
 
-            // 현재 부분 문자열 길이 갱신
             maxLength = Math.max(maxLength, right - left + 1);
         }
 
