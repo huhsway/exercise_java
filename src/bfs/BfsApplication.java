47aa539a9d1301633fbc75f53dec84f5f0ca940e
허승회 <35622185+huhsway@users.noreply.github.com>
2025-06-30 20:34:25 +0900

    Update BfsApplication.java

--- src/bfs/BfsApplication.java
+++ src/bfs/BfsApplication.java
@@ -5,31 +5,33 @@ import java.util.*;
 
 // https://yganalyst.github.io/training/algo_tr_ect_2/
 
+class Position {
+    public int y, x;
+    public Position(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+}
+
 public class BfsApplication {
 
-    private static class Position {
-        int y, x;
+    private int[][] graph;
+    private int n, m;
+    private final int[] dy = {-1, 1, 0, 0};
+    private final int[] dx = {0, 0, -1, 1};
 
-        public Position(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
+    public BfsApplication(int n, int m, int[][] graph) {
+        this.n = n;
+        this.m = m;
+        this.graph = graph;
     }
 
-    private static int[][] graph;
-    private static int n, m;
-    private static int[] dy = {-1, 1, 0, 0};
-    private static int[] dx = {0, 0, -1, 1};
-
-    private static int bfs(int y, int x) {
+    public int bfs(int y, int x) {
         Queue<Position> queue = new LinkedList<>();
-
-        // add()는 LinkedList가 고정 크기를 가지고 있다면 가득 차있을 때 예외를 throw하고 offer()는 LinkedList가 가득 차있을 때 즉시 false를 반환하고 예외를 throw하지 않습니다.
         queue.offer(new Position(y, x));
 
         while (!queue.isEmpty()) {
-            // poll() 메서드는 큐의 맨 앞에서 요소를 제거하고 반환합니다. 만약 큐가 비어있는 경우에는 null을 반환합니다.
-            Position pos = queue.poll(); // while문 안에서 꺼내자
+            Position pos = queue.poll();
             y = pos.y;
             x = pos.x;
 
@@ -47,17 +49,16 @@ public class BfsApplication {
                 }
             }
         }
-
         return graph[n - 1][m - 1];
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        graph = new int[n][m];
+        int[][] graph = new int[n][m];
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
@@ -65,7 +66,7 @@ public class BfsApplication {
             }
         }
 
-        System.out.println(bfs(0, 0));
+        BfsApplication app = new BfsApplication(n, m, graph);
+        System.out.println(app.bfs(0, 0));
     }
-
 }
