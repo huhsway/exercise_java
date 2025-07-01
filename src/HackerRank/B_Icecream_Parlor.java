c383e72520c74dedb27142564a3f3ed79d61b769
허승회 <35622185+huhsway@users.noreply.github.com>
2025-07-01 09:56:41 +0900

    Update and rename B_Ice_Cream_Parlor.java to B_Icecream_Parlor.java

--- src/HackerRank/B_Icecream_Parlor.java
+++ src/HackerRank/B_Icecream_Parlor.java
@@ -12,7 +12,7 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class Result {
+class B_Icecream_Parlor {
 
     /*
      * Complete the 'icecreamParlor' function below.
